c5a57f5d1cd4b994176eeaae0e02f79b9b058f3f
Davi Rodrigues <davirodr8@gmail.com>
2020-08-30 11:48:27 -0300

    corrigindo output

--- variable.tf
+++ variable.tf
@@ -9,16 +9,3 @@ variable "instance_type" {
   default     = "t2.micro"
   description = "Tipo de instância ec2 usada na AWS"
 }
-
-
-variable "enable_ids" {
-  type        = bool
-  default     = false
-  description = "(optional) describe your variable"
-}
-
-variable "ami_ids" {
-  type        = list(string)
-  default     = ["ami-0758470213bdd23b1"]
-  description = "(optional) describe your variable"
-}

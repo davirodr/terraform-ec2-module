5564564c037efacbfc4eca2e0549e1655039069f
Davi Rodrigues <davirodr8@gmail.com>
2020-08-30 00:53:31 -0300

    Private ip

--- output.tf
+++ output.tf
@@ -1,7 +1,12 @@
-output "ip_address" {
+output "public_address" {
   value = {
     for instance in aws_instance.web :
     instance.id => instance.public_ip
+  }
+}
+output "private_address" {
+  value = {
+    for instance in aws_instance.web :
     instance.id => instance.private_ip
   }
 }

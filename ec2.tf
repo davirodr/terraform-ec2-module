ff4e4606c922ed59a2f7ab3237c8fb4b16792fad
Davi Rodrigues <davirodr8@gmail.com>
2020-08-30 10:44:41 -0300

    Ubuntu owners 2

--- ec2.tf
+++ ec2.tf
@@ -6,7 +6,7 @@ data "aws_ami" "ubuntu" {
     values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
   }
 
-  owners = ["099720109477"] # Ubuntu
+  owners = ["099720109477"] # Ubuntu teste
 }
 
 resource "aws_instance" "web" {

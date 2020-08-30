720ace35934541b87b8dab6656263000b8be7d70
Davi Rodrigues <davirodr8@gmail.com>
2020-08-30 12:18:13 -0300

    corrigindo output

--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
 output "ip_public" {
-  value = "${aws_instance.web[*].public_ip}, ${aws_instance.web[*].private_ip}"
+  value = "${aws_instance.web[*].public_ip}"
+}
+
+output "ip_private" {
+  value = "${aws_instance.web[*].private_ip}"
 }

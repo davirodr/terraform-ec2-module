b9a7803206f8bdf5bb8d7c4eca88a08f46815e12
Davi Rodrigues <davirodr8@gmail.com>
2020-08-30 16:44:32 -0300

    Adicionando SG dinamico

--- ec2.tf
+++ ec2.tf
@@ -10,8 +10,8 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.instance_type
   vpc_security_group_ids = var.enable_sg ? aws_security_group.allow_tls[*].id : 1
   tags = {
     Name = var.app_name
--- sg.tf
+++ sg.tf
@@ -1,8 +1,20 @@
 resource "aws_security_group" "allow_tls" {
+  count       = var.enable_sg ? 1 : 0
   name        = "allow_tls"
   description = "Allow TLS inbound traffic"
   vpc_id      = aws_vpc.main.id
 
+
+  ingress {
+    interator = port
+    for_each  = var.list_sg
+    content {
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
   ingress {
     description = "22"
     from_port   = 22

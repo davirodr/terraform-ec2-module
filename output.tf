c9631c2667cf809a21126ba4443f0fdadfcb5200
Davi Rodrigues <davirodr8@gmail.com>
2020-08-30 11:58:26 -0300

    corrigindo output

--- output.tf
+++ output.tf
@@ -1,18 +1,3 @@
-output "ip_address" {
-  value = ["${aws_instance.web[*].public_ip}, ${aws_instance.web[*].private_ip}"]
+output "ip_address_public" {
+  value = "${aws_instance.web.public_ip}, ${aws_instance.web.id}, ${aws_instance.web.private_ip}"
 }
-/*
-output "public_address" {
-  value = {
-    for instance in aws_instance.web :
-    instance.id => instance.public_ip
-  }
-}
-output "private_address" {
-  value = {
-    for instance in aws_instance.web :
-    instance.id => instance.private_ip
-  }
-}
-
-*/

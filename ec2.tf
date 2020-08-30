41b2a54f00b0a377ac087c1fb0c8f09c3afa05bd
Davi Rodrigues <davirodr8@gmail.com>
2020-08-30 16:33:44 -0300

    Variavel para AMI

--- ec2.tf
+++ ec2.tf
@@ -10,7 +10,7 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "web" {
-  ami           = var.enable_sg ? var.list_sg[*] : [data.aws_ami.ubuntu.id]
+  ami           = var.enable_sg ? var.list_sg[*] : data.aws_ami.ubuntu.id
   instance_type = var.instance_type
  // vpc_security_group_ids = var.enable_sg ? aws_security_group.allow_tls[*].id : data.
   tags = {

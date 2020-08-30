45b0cdfdcf5c9128a4dfbabed7776354bc7ff863
Davi Rodrigues <davirodr8@gmail.com>
2020-08-30 00:46:20 -0300

    corrigindo erro

--- variable.tf
+++ variable.tf
@@ -9,20 +9,20 @@ variable "app_name" {
 }
 
 variable "instance_type" {
-  type = string
-  default = "t2.micro"
+  type        = string
+  default     = "t2.micro"
   description = "Tipo de instância ec2 usada na AWS"
 }
 
 
 variable "enable_ids" {
-  type = bool
-  default = false
+  type        = bool
+  default     = false
   description = "(optional) describe your variable"
 }
 
 variable "ami_ids" {
-  type = list(string)
-  default = ["ami-0758470213bdd23b1", "ami-0758470213bdd23b2"]
+  type        = list(string)
+  default     = ["ami-0758470213bdd23b1"]
   description = "(optional) describe your variable"
 }

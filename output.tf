6cceb5cac5a31ec89d8bccc4066f195d3b232cd6
Davi Rodrigues <davirodr8@gmail.com>
2020-08-30 15:19:23 -0300

    Variavel para AMI

--- output.tf
+++ output.tf
@@ -1,15 +1,15 @@
 
 output "ip_public" {
   value = "${aws_instance.web[*].public_ip}"
-  descrition = "saida com ip publico das instancias"
+  description = "saida com ip publico das instancias"
 }
 
 output "ip_private" {
   value = "${aws_instance.web[*].private_ip}"
-  descrition = "saida com ip privado das instancias"
+  description = "saida com ip privado das instancias"
 }
 
 output "instance_id" {
   value = "${aws_instance.web[*].id}"
-  descrition =  "saidas com os ids das instancias"
+  description =  "saidas com os ids das instancias"
 }

c24fd66751fc37180a72540fb3b9b67ce65b60ab
Davi Rodrigues <davirodr8@gmail.com>
2020-08-31 01:55:52 -0300

    atualizando

--- sg.tf
+++ sg.tf
@@ -1,19 +1,17 @@
-
-
 resource "aws_security_group" "sg_optional" {
- // count       = var.enable_sg ? 1 : 0
+  count       = var.enable_sg ? 1 : 0 : count.index <= 1 : 
   name        = "allow_tls-${var.name}"
   description = "Allow TLS inbound traffic"
 
 
   dynamic "ingress" {
-    iterator = port
-    for_each = var.list_sg
+    for_each = var.default_ingress
     content {
-      from_port   = port.value
-      to_port     = port.value
+      description = ingress.value["description"]
+      from_port   = ingress.key
+      to_port     = ingress.key
       protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
+      cidr_blocks = ingress.value["cidr_blocks"]
     }
   }
 

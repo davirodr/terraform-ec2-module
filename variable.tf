b97ecbd30002563fbcdbb915de9384c544a69409
Davi Rodrigues <davirodr8@gmail.com>
2020-08-31 01:56:40 -0300

    atualizando

--- variable.tf
+++ variable.tf
@@ -22,7 +22,7 @@ variable "name" {
   description = "(optional) describe your variable"
 }
 
-variable default_ingress {
+variable "default_ingress" {
   type = map(object({description = string, cidr_blocks = list(string)}))
   default = {
     22 = { description = "Inbound para SSH", cidr_blocks = [ "127.0.0.1/32" ] }
